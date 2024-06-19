a89632296ce7b0a1bb6e426f7cd80e8fef97169a
csl <1700138161@qq.com>
2024-06-19 23:34:11 +0800

    [new]通过laytpl渲染

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,44 @@
 const data={
     userName: "菜菜",
     job:"后端开发工程师",
+    address:"成都",
+    age:"24岁",
+    sex:"男",
+    tel:15984426676,
+    email:"notmastr@163.com",
+    skills:[
+        {
+            name:"JAVA",
+            per:88
+        },
+        {
+            name:"Golang",
+            per:88
+        },
+        {
+            name:"Linux",
+            per:85
+        },
+        {
+            name:"Mysql",
+            per:83
+        },
+        {
+            name:"Oracle",
+            per:80
+        },
+    ]
 }
 console.log("感谢您的浏览，期待能够加入贵公司！");
 layui.use(function(){
-    var layer = layui.layer;
-    var util = layui.util;
-    layer.msg('Hello World', {icon: 6});
-    // $("#userName").innerHTML = data.userName
-    // $("#job").innerHTML = data.job
+    var laytpl = layui.laytpl,$=layui.jquery
+    $('#view').html("渲染中...")
+    //引入外部html
+    $.get('assets/pages/home.html',function(homePage){
+        //渲染并输出结果
+        laytpl(homePage).render(data, function(str){
+            $('#view').html(str);
+          });
+    });
 })
 
